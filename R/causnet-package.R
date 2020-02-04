ab1e20e5580d34b3d4fe945ba44f62a2895bd322
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2020-02-04 15:04:56 -0800

    remove rcpp

--- R/causnet-package.R
+++
@@ -1,7 +0,0 @@
-# The following block is used by usethis to automatically manage
-# roxygen namespace tags. Modify with care!
-## usethis namespace: start
-#' @useDynLib causnet, .registration = TRUE
-#' @importFrom Rcpp sourceCpp
-## usethis namespace: end
-NULL
