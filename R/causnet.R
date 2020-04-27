b5b54c208b66050bb02ca6aca8bffcaffabdc11b
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2020-04-26 18:43:42 -0700

    allow custom scoring function

--- R/causnet.R
+++ R/causnet.R
@@ -4,6 +4,8 @@
 #' @param alpha Numerical, p-value threshold to determine cutoff for possible
 #'   parents.
 #' @param max_parents Numeric, maximal number of parents. Defaults to 2.
+#' @param score_fun A function used for scoring parent sets. For more details
+#' see score_fun vignette.
 #'
 #' @return A list with 2 elements: a data data.frame with 3 columns; from, to
 #' and component, and a vector of number of best parents.
@@ -13,14 +15,18 @@
 #' set.seed(1)
 #' new_data <- simdat(n_var = 5)
 #' out <- causnet(data = new_data, 0.5)
-causnet <- function(data, alpha = 0.05, max_parents = 2) {
+causnet <- function(data, alpha = 0.05, max_parents = 2, score_fun = NULL) {
 
   if (length(alpha) != 1 || !is.numeric(alpha) || alpha < 0 || alpha > 1) {
     stop("`alpha` must be a single numeric value in [0, 1].", call. = FALSE)
   }
 
+  if (is.null(score_fun)) {
+    score_fun <- score_bic_lm
+  }
+
   # node scores, no parents
-  ms <- no_parent_score(data)
+  ms <- no_parent_score(data, score_fun)
   possible_parents <- find_possible_parents(data, alpha)
 
   # possible offspring
@@ -32,7 +38,7 @@ causnet <- function(data, alpha = 0.05, max_parents = 2) {
 
   # scores for all sets of possible parents for each node
   possible_parent_set_scores <-
-    score_possible_parent_sets(data, possible_parent_sets)
+    score_possible_parent_sets(data, possible_parent_sets, score_fun)
 
   # BEST parent sets and scores for all sets of possible parents for each node
   bps1 <- best_possible_parent_sets(ms, possible_parent_sets,
--- tests/testthat/test-causnet.R
+++ tests/testthat/test-causnet.R
@@ -9,3 +9,12 @@ test_that("Input validation for causnet", {
   expect_error(causnet(data = new_data, -0.01), "`alpha` must")
   expect_error(causnet(data = new_data, 1.01), "`alpha` must")
 })
+
+test_that("custom scoring function argument works", {
+  mydata <- simdat(n_var = 5)
+
+  out <- causnet(mydata, score_fun = function(x, y, mydat) 0)
+
+  expect_equal(nrow(out$network), 0)
+  expect_equal(length(out$n_best_parents), 0)
+})
